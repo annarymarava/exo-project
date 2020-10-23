5d9a170a30dbe88617812131968f5148c08a6d49
Anna <arymarava@gmail.com>
2020-10-23 16:58:53 +0300

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+let burger_icon = document.querySelector('.burger-icon');
+let burger_menu = document.querySelector('.burger-menu');
+let back = document.querySelector('body');
+let header_list = document.querySelector('.header-list');
+
+burger_icon.onclick = function () {
+    burger_icon.classList.toggle('active');
+    burger_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+// // header_list.onclick = function () {
+// //     header_list.classList.remove('active');
+// //     back.classList.toggle('lock');
+// // }
+
+// // back.onclick = function () {
+// //     header_list.classList.remove('active');
+// //     back.classList.toggle('lock');
+// // }
