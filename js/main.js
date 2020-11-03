78d37fb3ac281b2498b4b797b850c991171d7599
Anna <arymarava@gmail.com>
2020-11-04 00:10:42 +0300

    Add fixed navigation and correct mistakes

--- js/main.js
+++ js/main.js
@@ -3,12 +3,44 @@ let burger_icon = document.querySelector('.burger-icon');
 let burger_menu = document.querySelector('.burger-menu');
 let back = document.querySelector('body');
 let header_list = document.querySelector('.header-list');
+let header_list_substrate = document.querySelector('.header-list-substrate');
+
 
 burger_icon.onclick = function () {
     burger_icon.classList.toggle('active');
     burger_menu.classList.toggle('active');
+    header_list_substrate.classList.toggle('active');
+    burger_icon.classList.remove('close');
+    burger_menu.classList.remove('close');
+    header_list_substrate.classList.remove('close');
     back.classList.toggle('lock');
 }
+
+header_list_substrate.onclick = function () {
+  burger_icon.classList.toggle('close');
+  burger_menu.classList.toggle('close');
+  header_list_substrate.classList.toggle('close');
+  burger_icon.classList.remove('active');
+  burger_menu.classList.remove('active');
+  header_list_substrate.classList.remove('active');
+  back.classList.toggle('lock');
+}
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 750) {
+      $('.header-body').addClass('fixed');
+  } else {
+      $('.header-body').removeClass('fixed');
+  }
+});
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 750) {
+      $('.header-list-substrate').addClass('fixed');
+  } else {
+      $('.header-list-substrate').removeClass('fixed');
+  }
+});
 
 // // header_list.onclick = function () {
 // //     header_list.classList.remove('active');
@@ -38,3 +70,5 @@ var swiper = new Swiper('.swiper-container-team', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
