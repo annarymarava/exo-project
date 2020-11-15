fd83eebe8bf1e52afbb736d3e5e2b37da044d3ae
Anna <arymarava@gmail.com>
2020-11-15 17:04:22 +0300

    correct slider and add link for team section

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ let burgerIcon = document.querySelector('.burger-icon');
 let burgerMenu = document.querySelector('.burger-menu');
 let body = document.querySelector('body');
 let headerListSubstrate = document.querySelector('.header-list-substrate');
+let html = document.querySelector('html');
 
 
 burgerIcon.addEventListener('click', function () {
@@ -11,7 +12,7 @@ burgerIcon.addEventListener('click', function () {
   burgerMenu.classList.remove('close');
   headerListSubstrate.classList.remove('close');
   body.style.paddingRight = bodyPadding + 'px';
-  body.classList.add('hidden-scroll');
+  html.classList.add('hidden-scroll');
   burgerMenu.classList.add('active');
   headerListSubstrate.classList.add('active');
 });
@@ -19,7 +20,7 @@ burgerIcon.addEventListener('click', function () {
 headerListSubstrate.addEventListener('click', function () {
   burgerMenu.classList.remove('active');
   headerListSubstrate.classList.remove('active');
-  body.classList.remove('hidden-scroll');
+  html.classList.remove('hidden-scroll');
   body.style.paddingRight = 0;
   burgerMenu.classList.add('close');
   headerListSubstrate.classList.add('close');
@@ -35,13 +36,14 @@ var swiper = new Swiper('.swiper-container-team', {
   slidesPerView: 1,
   spaceBetween: 29,
   freeMode: true,
+  freeModeSticky: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
-  breakpoints: { 
-    649: { slidesPerView: 2},
-    1249: { slidesPerView: 4}
+  breakpoints: {
+    649: { slidesPerView: 2 },
+    1249: { slidesPerView: 4 }
   }
 });
 
@@ -51,13 +53,13 @@ var swiper = new Swiper('.swiper-container-clients', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  
+
   nextButton: '#js-prev1',
   prevButton: '#js-next1',
-  breakpoints: { 
-    1000: { slidesPerView: 6},
-    500: { slidesPerView: 3},
-    400: { slidesPerView: 2}
+  breakpoints: {
+    1000: { slidesPerView: 6 },
+    500: { slidesPerView: 3 },
+    400: { slidesPerView: 2 }
   }
 });
 
@@ -66,44 +68,49 @@ var swiper = new Swiper('.swiper-container-pircing', {
   slidesPerView: 1,
   spaceBetween: 29,
   freeMode: true,
+  freeModeSticky: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   breakpoints: {
-    999: { slidesPerView: 3}
+    999: {
+      slidesPerView: 3
+    }
   }
 });
 
+
+
 let btnsCategories = document.querySelector('.categories');
 
-let removeClass = function(arr, className){
+let removeClass = function (arr, className) {
   arr
-  .filter(btn => btn.classList.contains(className))
-  .map(btn => btn.classList.remove(className));
+    .filter(btn => btn.classList.contains(className))
+    .map(btn => btn.classList.remove(className));
 };
 
-btnsCategories.addEventListener('click', function(e) {
-    if (!e.target.closest('li')) return;
-    let btn = e.target.closest('button');
-    if (btn.classList.contains('active-sorting')) return;
-    let btns = document.querySelectorAll('.categories button');
+btnsCategories.addEventListener('click', function (e) {
+  if (!e.target.closest('li')) return;
+  let btn = e.target.closest('button');
+  if (btn.classList.contains('active-sorting')) return;
+  let btns = document.querySelectorAll('.categories button');
 
 
-let arrBtns = Array.prototype.slice.call(btns);
+  let arrBtns = Array.prototype.slice.call(btns);
 
-    removeClass(arrBtns, 'active-sorting');
+  removeClass(arrBtns, 'active-sorting');
 
-    btn.classList.add('active-sorting');
+  btn.classList.add('active-sorting');
 
-    let btnId = btn.id;
-    let photoPortfolio = document.querySelectorAll('.photo-portfolio')
-    let arrPhotoPortfolio = Array.prototype.slice.call(photoPortfolio);
+  let btnId = btn.id;
+  let photoPortfolio = document.querySelectorAll('.photo-portfolio')
+  let arrPhotoPortfolio = Array.prototype.slice.call(photoPortfolio);
 
-    removeClass(arrPhotoPortfolio, 'visual-sorting');
-    if (btnId == 'all') return;
+  removeClass(arrPhotoPortfolio, 'visual-sorting');
+  if (btnId == 'all') return;
 
-    arrPhotoPortfolio
+  arrPhotoPortfolio
     .filter(photo => (photo.dataset.sorting != btnId))
     .map(photo => photo.classList.add('visual-sorting'));
 });
